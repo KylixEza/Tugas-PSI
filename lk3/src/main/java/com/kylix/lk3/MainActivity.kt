ffe1bf18c0271aec7da324cf3e3b8ff38e9fc3e8
ACER <loilyxtra@gmail.com>
2022-09-29 10:46:47 +0700

    init commit

---
+++ lk3/src/main/java/com/kylix/lk3/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.kylix.lk3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.kylix.lk3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity Lifecycle"
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        Log.i(TAG, "onCreate")
+    }
+    
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+    
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+    
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+}
